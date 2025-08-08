f9c0c47610a0a2159557e7f5f730552ac8569153
ryusei-hata <riyusei_hata@mail.toyota.co.jp>
2025-08-08 14:28:02 +0900

    Update main.js

--- sample/main.js
+++ sample/main.js
@@ -9,10 +9,11 @@ fetch(url)
        console.log(weather);
        const date = new Date(weather.reportDatetime);
        const formatDatetime = `${date.getFullYear()}/${String(date.getMonth() + 1).padStart(2, '0')}/${String(date.getDate()).padStart(2, '0')} ${String(date.getHours()).padStart(2, '0')}:${String(date.getMinutes()).padStart(2, '0')}:${String(date.getSeconds()).padStart(2, '0')}`;
+       const text = weather.text.replace(/\n\n/g, '<br>').replace(/　/g, '');
        // 画面に書き出す
        document.getElementById("publishingOffice").lastElementChild.textContent = weather.publishingOffice;
        document.getElementById("reportDatetime").lastElementChild.textContent = formatDatetime;
        document.getElementById("targetArea").lastElementChild.textContent = weather.targetArea;
        document.getElementById("headlineText").lastElementChild.textContent = weather.headlineText;
-       document.getElementById("text").lastElementChild.innerHTML = weather.text.replace(/\n\n/g, '<br>');
+       document.getElementById("text").lastElementChild.innerHTML = text;
    });
